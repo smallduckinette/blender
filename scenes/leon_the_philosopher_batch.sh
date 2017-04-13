ae9e8d2372a47cb4b06f3e3bacb13874bc2bbfdc
small_duck <small_duck@technoturtle.net>
2017-04-13 22:40:14 +0100

    Added credits

--- scenes/leon_the_philosopher_batch.sh
+++ scenes/leon_the_philosopher_batch.sh
@@ -5,3 +5,6 @@ blender -b leon_the_philosopher.blend -S Leon -a
 
 # Render the title
 blender -b leon_the_philosopher.blend -S Titre -a
+
+# Render the credits
+blender -b leon_the_philosopher.blend -S Credits -a
